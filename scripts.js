96bc4195c0ea257619ccf05ebd192f0d8ba801a9
alinecascaes <alinemcascaes@gmail.com>
2026-05-18 09:03:31 -0300

    adding currency

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+const USD = 5.06;
+const EUR = 5.89;
+const GBP = 6.77;
+
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
@@ -10,4 +14,18 @@ amount.addEventListener("input", () => {
 
 form.onsubmit = (e) => {
   e.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(anount.value, GBP, "£");
+      break;
+  }
 };
+
+function convertCurrency(amount, price, symbol) {}
