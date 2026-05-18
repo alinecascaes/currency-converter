6c62490ae4d187a0e33388d9a3387f06d239bf23
alinecascaes <alinemcascaes@gmail.com>
2026-05-18 09:42:11 -0300

    currency format

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 amount.addEventListener("input", () => {
   const hasRegex = /\D+/g;
@@ -24,17 +26,27 @@ form.onsubmit = (e) => {
       convertCurrency(amount.value, EUR, "€");
       break;
     case "GBP":
-      convertCurrency(anount.value, GBP, "£");
+      convertCurrency(amount.value, GBP, "£");
       break;
   }
 };
 
 function convertCurrency(amount, price, symbol) {
   try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+    result.textContent = `${amount * price} Reais`;
+
     footer.classList.add("show-result");
   } catch (error) {
     console.log(error);
     footer.classList.remove("show-result");
     alert("Não foi possível converter. Tente novamente mais tarde.");
   }
+}
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
